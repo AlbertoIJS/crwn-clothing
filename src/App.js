326fd4cea3a46b854f4d318c647736698ed09a0f
Alberto <albertoijs@outlook.com>
2019-10-23 11:22:38 -0500

    update the app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 // Redux connect
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -12,6 +12,7 @@ import HomePage from './pages/homepage/home-page.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
+import SingIn from './components/sign-in/sign-in.component';
 
 
 
@@ -48,16 +49,25 @@ class App extends Component{
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route 
+          path='/signin' 
+          render={() => 
+          this.props.currentUser ? 
+          (<Redirect to='/'/>) :
+          (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
 // Connects the App component with the user reducer
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from './user.types'; 
+
 const INITIAL_STATE = {
   currentUser : null
 }
@@ -13,12 +15,11 @@ const INITIAL_STATE = {
 // Check for user change
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       return{
         ...state, // Spreading all of the values
         currentUser: action.payload // Changing the value we care about
       }
-  
     default:
       return state;
   }
