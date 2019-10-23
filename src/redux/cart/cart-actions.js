dd2140a166f3f5b92ab5d6a00d359563130ca774
Alberto <albertoijs@outlook.com>
2019-10-23 17:01:46 -0500

    Update the shop data, cart utils and cart reducer

--- src/redux/cart/cart-actions.js
+++ src/redux/cart/cart-actions.js
@@ -1,5 +1,17 @@
+/*
+  ACTIONS:
+  1.- Describes what happend
+  2.- It is a payload of information
+*/
+
+// Types of actions
 import CartActionTypes from "./cart.types"
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 })
+
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+} )
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,12 @@
- const CartActionTypes = {
-  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+/*
+  ACTION TYPES:
+  1.- Can hide a cart 
+  2.- Can add an item
+*/ 
+ 
+const CartActionTypes = {
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM'
 }
 
 export default CartActionTypes;
